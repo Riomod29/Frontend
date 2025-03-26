7325da307e30fee3b1510d99b065db16b032dce8
NCT <186848012+Riomod29@users.noreply.github.com>
2025-03-26 21:45:12 +0700

    Add files via upload

--- Session_22_Array/Task_3/main.js
+++ Session_22_Array/Task_3/main.js
@@ -1,9 +1,9 @@
 let input = prompt("Nhập vào một dãy số: "); //Cho người dùng nhập vào
 let numberArray = input.split(''); //
-
+// Kiểm tra dãy số nhập vào có phải là dãy số không
 if (numberArray.some(isNaN)) {
     alert("Dãy không hợp lệ");
 };
-
+// Đảo ngược dãy số
 let reversedArray = numberArray.reverse();
 alert(reversedArray.join(""));
